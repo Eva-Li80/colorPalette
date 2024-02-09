2cb8fafda3dab54ee6c50f5a76034529aa0d167b
Eva-Li80 <evaliboden@gmail.com>
2024-02-09 15:18:38 +0100

    romeved som files

--- colors/src/Types_interfaces/types.tsx
+++ colors/src/Types_interfaces/types.tsx
@@ -3,24 +3,9 @@ export type HeadProps = {
     subtitle? : string
 }
 
-export type SearchColorProps = {
-    colors: string[];
-  }
-
-
-export type FootProps = {
-    title: string;
-}
-
 export type ButtonProps = {
     text: string;
     onClick?: () => void | boolean;
-}
-
-export type ButtonPropsAdmin = {
-    text?: string;
-    onClick: () => void;
-    children?: React.ReactNode;
 }
 
 export type TextCardProps = {
